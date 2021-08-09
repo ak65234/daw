5a01025fc2c47eb084deff42f4832257c183f888
AK <4609654-ak65234@users.noreply.gitlab.com>
2021-08-08 23:32:15 -0500

    Oscillator class

--- scripts/audio.js
+++ scripts/audio.js
@@ -62,3 +62,43 @@ class Gain extends Node {
         return this;
     }
 }
+
+class Oscillator extends Node {
+    /** @type {OscillatorNode} */
+    #osc;
+
+    /** @param {AudioContext} ctx */
+    constructor(ctx) {
+        const osc = ctx.createOscillator();
+        super(osc);
+        this.#osc = osc;
+    }
+
+    /** @param {OscillatorType} type */
+    type(type) {
+        this.#osc.type = type;
+
+        return this;
+    }
+
+    /** @param {number} frequency */
+    freq(frequency) {
+        this.#osc.frequency.value = frequency;
+
+        return this;
+    }
+
+    /** @param {number} time Time in seconds */
+    start(time = 0) {
+        this.#osc.start(time);
+
+        return this;
+    }
+
+    /** @param {number} time Time in seconds */
+    stop(time = 0) {
+        this.#osc.stop(time);
+
+        return this;
+    }
+}
